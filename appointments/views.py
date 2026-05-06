c81eb66fe732b762822941f24e0c313e3c5fa291
jestin16 <jestinshaji777@gmail.com>
2026-05-06 21:14:02 +0530

    Fix NameError in user_management by importing User model

--- appointments/views.py
+++ appointments/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.views import PasswordChangeView
